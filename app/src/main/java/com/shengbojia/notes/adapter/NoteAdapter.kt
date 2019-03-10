8cf7977ca092db19f7e7401ada26c3acd672cdfd
Shengbo Jia <shengbojia@gmail.com>
2019-03-10 16:06:05 -0400

    modified xml layouts

--- app/src/main/java/com/shengbojia/notes/adapter/NoteAdapter.kt
+++ app/src/main/java/com/shengbojia/notes/adapter/NoteAdapter.kt
@@ -2,24 +2,86 @@ package com.shengbojia.notes.adapter
 
 import android.content.Context
 import android.content.Intent
-import android.support.v7.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import com.shengbojia.notes.R
 import com.shengbojia.notes.data.Note
-import com.shengbojia.notes.ui.AddEditNoteActivity
+import com.shengbojia.notes.databinding.ItemNotesBinding
+import com.shengbojia.notes.ui.NoteListFragmentDirections
 import kotlinx.android.synthetic.main.item_notes.view.*
 
 /**
  * Adapter for the [RecyclerView] in [MainActivity]
  */
+
+class NoteAdapter : ListAdapter<Note, NoteAdapter.NoteHolder>(NoteDiffCallBack()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteHolder =
+        NoteHolder(ItemNotesBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        ))
+
+    override fun onBindViewHolder(holder: NoteHolder, position: Int) {
+        val note = getItem(position)
+        note.let {
+            with(holder) {
+                this.itemView.tag = it
+                bind(createOnClickListener(it.id), it)
+            }
+        }
+    }
+
+    private fun createOnClickListener(noteId: Int?): View.OnClickListener {
+        if (noteId != null) {
+            return View.OnClickListener {
+                Log.d(TAG, "You pressed note $noteId")
+                val direction = NoteListFragmentDirections.actionNoteListFragmentToEditNoteFragment(noteId)
+                it.findNavController().navigate(direction)
+            }
+        } else throw IllegalStateException("Current note not found")
+    }
+
+    class NoteHolder(
+        private val binding: ItemNotesBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(listener: View.OnClickListener, item: Note) {
+            binding.apply {
+                clickListener = listener
+                note = item
+                executePendingBindings()
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "AdapterNote"
+    }
+}
+
+private class NoteDiffCallBack : DiffUtil.ItemCallback<Note>() {
+
+    override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+        return oldItem == newItem
+    }
+}
+/*
 class NoteAdapter internal constructor(
     private val context: Context
-): RecyclerView.Adapter<NoteAdapter.NoteHolder>() {
+): androidx.recyclerview.widget.RecyclerView.Adapter<NoteAdapter.NoteHolder>() {
 
-    private val inflater = LayoutInflater.from(context)
+    //private val inflater = LayoutInflater.from(context)
     private var notes = listOf<Note>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteHolder {
@@ -42,7 +104,7 @@ class NoteAdapter internal constructor(
         notifyDataSetChanged()
     }
 
-    inner class NoteHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+    inner class NoteHolder(itemView: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(itemView), View.OnClickListener {
         internal val titleTv = itemView.tv_note_title
         internal val descriptionTv = itemView.tv_note_desc
         internal val priorityTv = itemView.tv_note_priority
@@ -57,7 +119,7 @@ class NoteAdapter internal constructor(
 
             Log.d(TAG, "onClick worked")
 
-            if (adapterPosition != RecyclerView.NO_POSITION) {
+            if (adapterPosition != androidx.recyclerview.widget.RecyclerView.NO_POSITION) {
 
                 Log.d(TAG, "at position: $adapterPosition")
 
@@ -92,3 +154,5 @@ class NoteAdapter internal constructor(
     }
 
 }
+
+        */
