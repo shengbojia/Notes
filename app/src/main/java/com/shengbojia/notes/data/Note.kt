57ab8be6d6bcdbe9cec52e79b5d4e1ad3470976e
Shengbo Jia <shengbojia@gmail.com>
2019-03-18 17:56:16 -0400

    added index to Note entity, to be more efficient

--- app/src/main/java/com/shengbojia/notes/data/Note.kt
+++ app/src/main/java/com/shengbojia/notes/data/Note.kt
@@ -2,13 +2,17 @@ package com.shengbojia.notes.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import java.util.*
 
 /**
  * [Note]
  */
-@Entity(tableName = "note_table")
+@Entity(
+    tableName = "note_table",
+    indices = [Index("id")]
+)
 data class Note(
     @PrimaryKey(autoGenerate = true) var id: Int? = null,
     val title: String,
