44a9e6318dc47d86fee4fa334f3abc7545829867
unknown <shengbojia@gmail.com>
2019-03-31 18:09:52 -0400

    new custom scrollview to prevent focus jumping

--- app/src/main/java/com/shengbojia/notes/ui/FixedFocusScrollView.kt
+++ app/src/main/java/com/shengbojia/notes/ui/FixedFocusScrollView.kt
@@ -1,4 +1,33 @@
 package com.shengbojia.notes.ui
 
-class FixedFocusScrollView {
+import android.annotation.TargetApi
+import android.content.Context
+import android.graphics.Rect
+import android.os.Build
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ScrollView
+import java.util.ArrayList
+
+class FixedFocusScrollView : ScrollView {
+
+    @JvmOverloads
+    constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0)
+            : super(context, attrs, defStyleAttr)
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int)
+            : super(context, attrs, defStyleAttr, defStyleRes)
+
+    override fun requestChildFocus(child: View?, focused: View?) {
+
+
+    }
 }
