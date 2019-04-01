f36f918407908809b6eb73a57a55f3645cde8181
unknown <shengbojia@gmail.com>
2019-04-01 00:36:21 -0400

    date format added

--- app/src/main/java/com/shengbojia/notes/adapter/BindingAdapters.kt
+++ app/src/main/java/com/shengbojia/notes/adapter/BindingAdapters.kt
@@ -3,6 +3,8 @@ package com.shengbojia.notes.adapter
 import android.widget.NumberPicker
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import java.text.DateFormat
+import java.util.*
 
 /**
  * Binds the displayed priority for each [CardView] in [NotesListFragment].
@@ -23,5 +25,14 @@ fun bindCurrentPriority(view: NumberPicker, priority: Int?) {
         view.minValue = 1
         view.maxValue = 5
         view.value = priority
+    }
+}
+
+@BindingAdapter("app:writtenDate")
+fun bindWrittenDate(view: TextView, dateWritten: Calendar?) {
+
+    if (dateWritten != null) {
+        val date = DateFormat.getDateInstance(DateFormat.LONG).format(dateWritten.time)
+        view.text = date
     }
 }
