aebe5240089cde2bb22c6c653921e656adc62819
unknown <shengbojia@gmail.com>
2019-05-17 01:19:35 -0400

    Some queries now return the # of rows affected

--- app/src/main/java/com/shengbojia/notes/data/NoteDao.kt
+++ app/src/main/java/com/shengbojia/notes/data/NoteDao.kt
@@ -10,22 +10,55 @@ import androidx.room.*
 @Dao
 interface NoteDao {
 
-    @Insert
+    /**
+     * Insert a note into the database. If the note already exists, replace it.
+     *
+     * @param note the note to insert
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(note: Note)
 
+    /**
+     * Update a note.
+     *
+     * @param note the note to update
+     * @return the number of notes updated. Should be 1
+     */
     @Update
-    fun update(note: Note)
+    fun update(note: Note): Int
 
-    @Delete
-    fun delete(note: Note)
+    /**
+     * Delete a note by id.
+     *
+     * @param noteId the id of the note to delete
+     * @return the number of notes deleted. Should be 1
+     */
+    @Query("DELETE FROM note_table WHERE id = :noteId")
+    fun delete(noteId: Int): Int
 
+    /**
+     * Delete all notes in the database.
+     *
+     * @return the total number of notes deleted
+     */
     @Query("DELETE FROM note_table")
-    fun deleteAllNotes()
+    fun deleteAllNotes(): Int
 
+    /**
+     * Select a note by id.
+     *
+     * @param noteId the id of the note to be selected
+     * @return the selected note
+     */
     @Query("SELECT * FROM note_table WHERE id = :noteId")
-    fun getNote(noteId: Int): LiveData<Note>
+    fun getNote(noteId: Int): Note?
 
+    /**
+     * Select all notes from note_table.
+     *
+     * @return the list of notes
+     */
     @Query("SELECT * FROM note_table ORDER BY date_written DESC")
-    fun getAllNotes(): LiveData<List<Note>>
+    fun getAllNotes(): List<Note>
 
 }
