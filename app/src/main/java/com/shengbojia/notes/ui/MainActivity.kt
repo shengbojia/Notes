3da10d9603b6f739ce7fe52ddab4c04ab2eb1b90
unknown <shengbojia@gmail.com>
2019-05-21 00:27:47 -0400

    optimized setting up the nav controller

--- app/src/main/java/com/shengbojia/notes/ui/MainActivity.kt
+++ app/src/main/java/com/shengbojia/notes/ui/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.shengbojia.notes.ui
 
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.shengbojia.notes.R
 import com.shengbojia.notes.databinding.ActivityMainBinding
 
@@ -30,39 +21,16 @@ class MainActivity : AppCompatActivity() {
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         navController = Navigation.findNavController(this, R.id.nav_host_frag)
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
 
         // Set up ActionBar
-
         setSupportActionBar(binding.toolbarMain)
-        binding.toolbarMain.setupWithNavController(navController, appBarConfiguration)
-
-
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
-
-    /*
-
-    private fun dialogConfirmation() {
-        val builder = AlertDialog.Builder(this, R.style.ConfirmationDialogStyle)
-        builder.apply {
-            setTitle(getString(R.string.dialog_confirmationTitle))
-            setPositiveButton(getString(R.string.dialog_positive)) { _, _ ->
-                noteViewModel.deleteAllNotes()
-            }
-            setNegativeButton(getString(R.string.dialog_cancel)) { _, _ ->
-            }
-        }
-        val dialog = builder.create()
-        dialog.show()
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
-    private fun onFabClick(view: View) {
-        val addIntent = Intent(this, AddEditNoteActivity::class.java)
-        startActivity(addIntent)
-    }
-
-    */
 
     companion object {
         private const val TAG = "ActMain"
