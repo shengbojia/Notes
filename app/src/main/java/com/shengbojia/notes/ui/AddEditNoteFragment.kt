c6318949b0cb27f107d91a9427bb03aa83e2726e
unknown <shengbojia@gmail.com>
2019-05-25 20:22:13 -0400

    unified fragment for editing and adding

--- app/src/main/java/com/shengbojia/notes/ui/AddEditNoteFragment.kt
+++ app/src/main/java/com/shengbojia/notes/ui/AddEditNoteFragment.kt
@@ -6,13 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-import com.shengbojia.uwaterlooinfosession.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.shengbojia.notes.R
 
 /**
  * A simple [Fragment] subclass.
