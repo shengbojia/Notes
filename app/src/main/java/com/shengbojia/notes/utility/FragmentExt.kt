0e351b6997fc717f51da5ff610f5eef44f6b6151
unknown <shengbojia@gmail.com>
2019-05-25 20:23:12 -0400

    cleaned up utils

--- app/src/main/java/com/shengbojia/notes/utility/FragmentExt.kt
+++ app/src/main/java/com/shengbojia/notes/utility/FragmentExt.kt
@@ -1,2 +1,12 @@
 package com.shengbojia.notes.utility
 
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+import com.shengbojia.notes.viewmodel.ViewModelFactory
+
+fun <T : ViewModel> Fragment.obtainViewModel(viewModelClass: Class<T>): T {
+
+    val repository = InjectorUtils.getNoteRepository(this.requireContext())
+    return ViewModelProviders.of(this, ViewModelFactory(repository)).get(viewModelClass)
+}
--- app/src/main/java/com/shengbojia/notes/utility/InjectorUtils.kt
+++ app/src/main/java/com/shengbojia/notes/utility/InjectorUtils.kt
@@ -1,49 +1,16 @@
 package com.shengbojia.notes.utility
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
-import com.shengbojia.notes.adapter.NoteAdapter
-import com.shengbojia.notes.data.db.AppDatabase
 import com.shengbojia.notes.data.NoteRepository
-import com.shengbojia.notes.ui.actionmode.MainActionModeCallback
-import com.shengbojia.notes.viewmodel.*
+import com.shengbojia.notes.data.db.AppDatabase
 
 /**
  * Static methods used to inject classes needed for various Activities and Fragments.
  */
 object InjectorUtils {
 
-    private fun getNoteRepository(context: Context): NoteRepository =
+    fun getNoteRepository(context: Context): NoteRepository =
             NoteRepository.getInstance(
                 AppDatabase.getInstance(context.applicationContext).noteDao()
             )
-
-    fun provideAddNoteViewModelFactory(context: Context): AddNoteViewModelFactory {
-        val repository = getNoteRepository(context)
-        return AddNoteViewModelFactory(repository)
-    }
-
-    fun provideEditNoteViewModelFactory(context: Context, noteId: Int): EditNoteViewModelFactory {
-        val repository = getNoteRepository(context)
-        return EditNoteViewModelFactory(repository, noteId)
-    }
-
-    fun provideNoteListViewModelFactory(context: Context): NoteListViewModelFactory {
-        val repository = getNoteRepository(context)
-        return NoteListViewModelFactory(repository)
-    }
-
-    fun provideDeleteNoteViewModelFactory(context: Context): DeleteNoteViewModelFactory {
-        val repository = getNoteRepository(context)
-        return DeleteNoteViewModelFactory(repository)
-    }
-
-    fun provideAdapterWithActionMode(activity: AppCompatActivity, viewModel: DeleteNoteViewModel): NoteAdapter {
-
-        val actionModeCallback = MainActionModeCallback(viewModel, activity)
-        val adapter = NoteAdapter(actionModeCallback)
-        actionModeCallback.adapter = adapter
-
-        return adapter
-    }
 }
