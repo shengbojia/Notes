c7e38157576bc3e79c9346300485ab500cfddad8
Shengbo Jia <shengbojia@gmail.com>
2019-08-04 01:29:19 -0400

    updated some deps

---
+++ app/src/test/java/com/shengbojia/notes/notelist/NoteListViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.shengbojia.notes.notelist
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class NoteListViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
