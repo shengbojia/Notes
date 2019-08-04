07d68da7b63d932769662ee118f98fb3f6010ea2
Shengbo Jia <shengbojia@gmail.com>
2019-08-04 01:30:47 -0400

    new file for testing notelist viewmodel

--- app/src/test/java/com/shengbojia/notes/notelist/NoteListViewModelTest.kt
+++ app/src/test/java/com/shengbojia/notes/notelist/NoteListViewModelTest.kt
@@ -1,14 +1,48 @@
 package com.shengbojia.notes.notelist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.shengbojia.notes.ViewModelScopeMainDispatcherRule
+import com.shengbojia.notes.data.Note
+import com.shengbojia.notes.data.NoteRepository
+import com.shengbojia.notes.data.Repository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineContext
 import org.junit.After
 import org.junit.Before
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.mockito.Mockito.mock
 
 class NoteListViewModelTest {
 
+    // View model to be tested
+    private lateinit var noteListViewModel: NoteListViewModel
+
+    // Mock repository
+    private val mockRepository: Repository = mock(Repository::class.java)
+
+    // CoroutineContext that can be controlled during tests
+    private val testContext = TestCoroutineContext()
+
+    // Set the main coroutine dispatcher to be the test one
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var coroutineMainDispatcherRule = ViewModelScopeMainDispatcherRule(testContext)
+
+    // Swaps background executor used by the Architecture Components with a different
+    // one that executes each task synchronously
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+
     @Before
-    fun setUp() {
+    fun setUpNoteListViewModel() {
+        val note1 = Note("Title1", "Desc1")
+        val note2 = Note("Title2", "Desc2")
+        val note3 = Note("Title3", "Desc3")
+
+        mockRepository.insert()
     }
 
     @After
