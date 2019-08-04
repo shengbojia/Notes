faa425867a46c4f335853b21b50614c6429f0e41
Shengbo Jia <shengbojia@gmail.com>
2019-08-04 02:48:47 -0400

    more testing of view model

--- app/src/test/java/com/shengbojia/notes/notelist/NoteListViewModelTest.kt
+++ app/src/test/java/com/shengbojia/notes/notelist/NoteListViewModelTest.kt
@@ -1,26 +1,27 @@
 package com.shengbojia.notes.notelist
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import com.shengbojia.notes.LiveDataTestUtil
+import com.shengbojia.notes.R
 import com.shengbojia.notes.ViewModelScopeMainDispatcherRule
+import com.shengbojia.notes.assertSnackbarMessage
+import com.shengbojia.notes.data.FakeRepository
 import com.shengbojia.notes.data.Note
-import com.shengbojia.notes.data.NoteRepository
-import com.shengbojia.notes.data.Repository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineContext
-import org.junit.After
-import org.junit.Before
 
-import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
-import org.mockito.Mockito.mock
+import org.junit.Test
 
 class NoteListViewModelTest {
 
     // View model to be tested
-    private lateinit var noteListViewModel: NoteListViewModel
+    private lateinit var viewModel: NoteListViewModel
 
-    // Mock repository
-    private val mockRepository: Repository = mock(Repository::class.java)
+    // Fake repository, I didn't use a mock here because there's some details to work out
+    private lateinit var fakeRepository: FakeRepository
 
     // CoroutineContext that can be controlled during tests
     private val testContext = TestCoroutineContext()
@@ -38,14 +39,57 @@ class NoteListViewModelTest {
 
     @Before
     fun setUpNoteListViewModel() {
+
+        // Setup a fresh repo before every test
+        fakeRepository = FakeRepository()
+
         val note1 = Note("Title1", "Desc1")
         val note2 = Note("Title2", "Desc2")
         val note3 = Note("Title3", "Desc3")
 
-        mockRepository.insert()
+        fakeRepository.addNotes(note1, note2, note3)
+
+        viewModel = NoteListViewModel(fakeRepository)
     }
 
-    @After
-    fun tearDown() {
+    @Test
+    fun getAllNotesFromRepository_dataLoadingTogglesAndDataLoaded() {
+        // Given an initialized NoteListViewModel and initialized + inserted notes
+
+        // When loading of all notes is requested
+        viewModel.getAllNotes()
+
+        // Then data loading indicator is shown
+        assertThat(LiveDataTestUtil.getValue(viewModel.dataLoading)).isTrue()
+
+        // Execute pending coroutine actions
+        testContext.triggerActions()
+
+        // Then data loading indicator becomes hidden
+        assertThat(LiveDataTestUtil.getValue(viewModel.dataLoading)).isFalse()
+
+        // And data was correctly loaded
+        assertThat(LiveDataTestUtil.getValue(viewModel.notes)).hasSize(3)
+    }
+
+    @Test
+    fun getAllNotesFromRepository_error() {
+        // Given repository that will return an error
+        fakeRepository.setShouldReturnError(true)
+
+        // When loading of all notes is requested
+        viewModel.getAllNotes()
+
+        // Execute pending coroutine actions
+        testContext.triggerActions()
+
+        // Then data loading error indicator is true
+        assertThat(LiveDataTestUtil.getValue(viewModel.isDataLoadingError)).isTrue()
+
+        // And list of notes is empty
+        assertThat(LiveDataTestUtil.getValue(viewModel.notes)).isEmpty()
+
+        // And snackbar text is updated with correct error message
+        assertSnackbarMessage(viewModel.snackBarText, R.string.snackbar_error_loading)
     }
 }
