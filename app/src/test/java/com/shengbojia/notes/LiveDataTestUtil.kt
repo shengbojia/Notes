2db254eab7940116a87084136b0b3f5a99744f25
Shengbo Jia <shengbojia@gmail.com>
2019-08-04 01:44:48 -0400

    added util functions for live data while testing

---
+++ app/src/test/java/com/shengbojia/notes/LiveDataTestUtil.kt
@@ -0,0 +1,33 @@
+package com.shengbojia.notes
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+/**
+ * Useful utility functions for checking the state of a [LiveData] object when doing testing.
+ */
+object LiveDataTestUtil {
+
+    /**
+     * Get the value from a LiveData object. We're waiting for LiveData to emit, for 2 seconds.
+     * Once we got a notification via onChanged, we stop observing.
+     */
+    fun <T> getValue(liveData: LiveData<T>): T {
+        val data = arrayOfNulls<Any>(1)
+        val latch = CountDownLatch(1)
+        val observer = object : Observer<T> {
+            override fun onChanged(o: T?) {
+                data[0] = o
+                latch.countDown()
+                liveData.removeObserver(this)
+            }
+        }
+        liveData.observeForever(observer)
+        latch.await(2, TimeUnit.SECONDS)
+
+        @Suppress("UNCHECKED_CAST")
+        return data[0] as T
+    }
+}
