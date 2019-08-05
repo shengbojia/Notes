a3e9f9198db766f1ce8826435d7af49c5dab32c9
Shengbo Jia <shengbojia@gmail.com>
2019-08-05 12:53:05 -0400

    more espresso testing for note list frag

--- app/src/sharedTest/java/com/shengbojia/notes/notelist/NoteListFragmentTest.kt
+++ app/src/sharedTest/java/com/shengbojia/notes/notelist/NoteListFragmentTest.kt
@@ -5,16 +5,22 @@ import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.shengbojia.notes.MainActivity
 import com.shengbojia.notes.R
 import com.shengbojia.notes.ServiceLocator
 import com.shengbojia.notes.data.FakeRepository
+import com.shengbojia.notes.data.Note
 import com.shengbojia.notes.data.Repository
+import com.shengbojia.notes.utility.insertNoteBlocking
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
@@ -73,6 +79,35 @@ class NoteListFragmentTest {
                 null, getApplicationContext<Context>().getString(R.string.fragment_title_addNote)
             )
         )
+
+    }
+
+    @Test
+    fun displayNoteList_whenRepositoryNotEmpty() {
+        // Given one note already in the repository
+        repository.insertNoteBlocking(Note("title1", "desc1"))
+
+        // When starting up app
+        launch(MainActivity::class.java)
+
+        // Then verify the note is visibly displayed on screen
+        onView(withText("title1")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun deleteOneNote() {
+        repository.insertNoteBlocking(Note("title1", "desc1"))
+
+        launch(MainActivity::class.java)
+
+        // Open the details of the inserted note
+        onView(withText("title1")).perform(click())
+
+        // Click the delete icon on the action bar
+        onView(withId(R.id.action_delete)).perform(click())
+
+        // Then verify that the note was deleted
+        onView(withText("title1")).check(doesNotExist())
 
     }
 }
