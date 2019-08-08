e9f0fe4fe8a57fc44cf49b395e92996f3a21c6d6
Shengbo Jia <shengbojia@gmail.com>
2019-08-07 20:32:34 -0400

    ui testing revealled error in two-way databinding, has been fixed.

--- app/src/sharedTest/java/com/shengbojia/notes/noteaddedit/AddEditNoteFragmentTest.kt
+++ app/src/sharedTest/java/com/shengbojia/notes/noteaddedit/AddEditNoteFragmentTest.kt
@@ -2,26 +2,33 @@ package com.shengbojia.notes.noteaddedit
 
 import android.content.Context
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.clearText
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import com.shengbojia.notes.R
 import com.shengbojia.notes.ServiceLocator
 import com.shengbojia.notes.data.FakeRepository
 import com.shengbojia.notes.data.Repository
+import com.shengbojia.notes.data.Result
+import com.shengbojia.notes.utility.getAllNotesBlocking
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
 import org.robolectric.annotation.LooperMode
 import org.robolectric.annotation.TextLayoutMode
 
@@ -64,6 +71,38 @@ class AddEditNoteFragmentTest {
 
         // And snackbar pops up with warning message
         onView(withId(R.id.snackbar_text)).check(matches(withText(R.string.snackbar_message_cannotBeEmpty)))
+    }
+
+    @Test
+    fun validNote_isSaved() {
+        // Given - starting on add fragment in isolation
+        val navController = mock(NavController::class.java)
+        setupFragment(navController)
+
+        // When saving a valid note with nonempty title and description
+        val title = "title"
+        val desc = "desc"
+        onView(withId(R.id.et_edit_title)).perform(replaceText(title))
+        onView(withId(R.id.et_edit_desc)).perform(replaceText(desc))
+        onView(withId(R.id.fab_addEdit_save)).perform(click())
+
+        // Then verify the note has been saved in the repository
+        val notes = (repository.getAllNotesBlocking() as Result.Success).data
+        assertThat(notes).hasSize(1)
+        assertThat(notes[0].title).isEqualTo(title)
+        assertThat(notes[0].description).isEqualTo(desc)
+    }
+
+    /**
+     * Helper method for setting up a [AddEditNoteFragment] with a [NavController].
+     */
+    private fun setupFragment(navController: NavController?) {
+        val bundle = AddEditNoteFragmentArgs(null,
+            getApplicationContext<Context>().getString(R.string.fragment_title_addNote)).toBundle()
+        val scenario = launchFragmentInContainer<AddEditNoteFragment>(bundle, R.style.AppTheme)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
     }
 
 }
